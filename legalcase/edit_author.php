016471bb264f7e1b89afc9786ddb4728c3f69968
Mathieu Lutfy <mathieu@bidon.ca>
2005-01-10 12:58:08 +0000

    removed second 'validate' button: for new user, it makes no sense..

--- legalcase/edit_author.php
+++ legalcase/edit_author.php
@@ -187,19 +187,11 @@ else lcm_page_start("New author");
 				</div>
 			</td>
 		</tr>
-		<tr><td colspan="2" align="center" valign="middle">
-			<input name="submit" type="submit" class="search_form_btn" id="submit" value="<?php echo _T('button_validate') ?>" /></td>
-		</tr>
-	</table>
-
-	<br />
-
 	<?php
 		//
 		// LOGIN INFO
 		//
 	?>
-	<table width="99%" border="0" align="center" cellpadding="5" cellspacing="0" class="tbl_usr_dtl">
 		<tr>
 			<td colspan="2" align="center" valign="middle" class="heading"><h4><?php echo _T('authoredit_subtitle_connectionidentifiers'); ?></h4></td>
 		</tr>
