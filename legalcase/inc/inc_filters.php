1190964269c5f0eb851684ea3c33f594442ced17
Anastas Giokov <antzi@antzi.bg>
2005-01-21 00:51:07 +0000

    Fixed format_date date_wday always 0 bug

--- legalcase/inc/inc_filters.php
+++ legalcase/inc/inc_filters.php
@@ -70,7 +70,10 @@ function format_date($timestamp = '', $format = 'full') {
 		return '';
 
 	$dd = recup_date($timestamp);
-	$day_of_w = strftime("%u", mktime(0, 0, 0, $dd[1], $dd[2], $dd[0]));
+//	$day_of_w = strftime("%u", mktime(0, 0, 0, $dd[1], $dd[2], $dd[0]));
+// [AG] The above always returns 0 for me (Windows 2K server?).
+// The following works, but Sunday is day 0 instead of 7
+	$day_of_w = date("w", mktime(0, 0, 0, $dd[1], $dd[2], $dd[0]));
 
 	$my_date = _T('date_format_' . $format, array(
 				'day_name' => _T('date_wday_' . ($day_of_w + 0)),
