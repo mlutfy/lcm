56e7ae39344e3cb2c0f9e498829aa8a122036362
Mathieu Lutfy <mathieu@bidon.ca>
2005-02-01 19:02:17 +0000

    added variable DEST to make life easier

--- doc/templates/make_help.sh
+++ doc/templates/make_help.sh
@@ -4,10 +4,11 @@
 # Files are seperated by language.
 
 DOCROOT="http://www.lcm.ngo-bg.org/rubrique_help.php"
+DEST="/var/www/legalcase/inc/help"
 
 if [ "X$1" = "X" -a "X$2" = "X" -a "X$3" = "X" ]; then
 	wget -q -O /dev/stdout $DOCROOT | sort | grep http | xargs -n 3 ./make_help.sh
 else
 	mkdir -p $2
-	wget -q -O "./$2/$1.html" "$3" 
+	wget -q -O "$DEST/$2/$1.html" "$3" 
 fi
