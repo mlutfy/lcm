6b83b52bbc54015a18ea24ea343a6e8c3ce90942
Mathieu Lutfy <mathieu@bidon.ca>
2005-02-02 08:43:37 +0000

    modified 'puce' (-) for 'li'

--- doc/templates/article_help.php
+++ doc/templates/article_help.php
@@ -6,6 +6,14 @@ $delais = 0;
 // Don't show the "refresh" buttons
 $flag_preserver = true;
 
+//
+// Customize the generated HTML
+//
+
+// For <li> elements
+// $GLOBALS['puce'] = '<img src="images/lcm/lcm_right_arrow.gif" alt="-" height="7" width="4" />';
+$GLOBALS['puce'] = '<li>';
+
 include ("inc-public.php3");
 
 ?>
