6e7f654b4f5c923dce70f27f4e4fee67bf65aff9
Mathieu Lutfy <mathieu@bidon.ca>
2005-02-02 08:24:28 +0000

    DEST can be set from the command line + fixed bug in mkdir which didn't respect DEST

--- doc/templates/make_help.sh
+++ doc/templates/make_help.sh
@@ -4,11 +4,21 @@
 # Files are seperated by language.
 
 DOCROOT="http://www.lcm.ngo-bg.org/rubrique_help.php"
-DEST="/var/www/legalcase/inc/help"
+
+# You can also pass DEST from the command line
+# DEST=/var/www/foo/inc/help ./make_help.sh
+if [ "x$DEST" = "x" ]; then
+	DEST="/var/www/legalcase/inc/help"
+fi
+
+if [ ! -d "$DEST" ]; then
+	echo "$DEST: destination directory does not exist. Aborting."
+	exit 1;
+fi
 
 if [ "X$1" = "X" -a "X$2" = "X" -a "X$3" = "X" ]; then
 	wget -q -O /dev/stdout $DOCROOT | sort | grep http | xargs -n 3 ./make_help.sh
 else
-	mkdir -p $2
+	mkdir -p "$DEST/$2"
 	wget -q -O "$DEST/$2/$1.html" "$3" 
 fi
