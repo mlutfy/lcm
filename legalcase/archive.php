aad079f13da6140280d63e82a8f2872ee1453391
Mathieu Lutfy <mathieu@bidon.ca>
2005-02-24 16:22:12 +0000

    added translation strings + minor cleanup of commented code

--- legalcase/archive.php
+++ legalcase/archive.php
@@ -30,20 +30,14 @@ if ($GLOBALS['author_session']['status'] != 'admin')
 	die("You don't have the right to list all cases!");
 
 // Show page start
-lcm_page_start("Archives");
+lcm_page_start(_T('title_archives'));
 
 // Show tabs
-$tabs = array(	array('name' => 'All cases','url' => 'archive.php'),
-		array('name' => 'Export DB','url' => 'export_db.php'),
-		array('name' => 'Import DB','url' => 'import_db.php')
+$tabs = array(	array('name' => _T('archives_tab_all_cases'), 'url' => 'archive.php'),
+		array('name' => _T('archives_tab_export'), 'url' => 'export_db.php'),
+		array('name' => _T('archives_tab_import'), 'url' => 'import_db.php')
 	);
 show_tabs_links($tabs,0);
-
-// This should be shown in other tabs
-/*echo "<p>This will go into tabs: ";
-echo '<a href="export_db.php" class="content_link">' . "Export DB" . "</a>, ";
-echo '<a href="import_db.php" class="content_link">' . "Import DB" . "</a>";
-echo "</p>\n";*/
 
 $q = "SELECT DISTINCT lcm_case.id_case,title,status,public,pub_write
 		FROM lcm_case,lcm_case_author
