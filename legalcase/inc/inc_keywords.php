ebe7ef93b5b7296a82d247828630f4c8175ab6f7
Mathieu Lutfy <mathieu@bidon.ca>
2005-03-03 16:10:04 +0000

    get_kw[g]_from_id(): added a bit of checking to avoid strange bugs

--- legalcase/inc/inc_keywords.php
+++ legalcase/inc/inc_keywords.php
@@ -59,6 +59,9 @@ function get_kwg_from_id($id_group) {
 				WHERE id_group = " . intval($id_group);
 	$result = lcm_query($query);
 
+	if (! lcm_num_rows($result))
+		lcm_panic("Invalid keyword group (ID = " . $id_group . ")");
+
 	return lcm_fetch_array($result);
 }
 
@@ -72,6 +75,9 @@ function get_kw_from_id($id_keyword) {
 				WHERE kwg.id_group = k.id_group
 				AND id_keyword = " . intval($id_keyword);
 	$result = lcm_query($query);
+
+	if (! lcm_num_rows($result))
+		lcm_panic("Invalid keyword (ID = " . $id_keyword . ")");
 
 	return lcm_fetch_array($result);
 }
