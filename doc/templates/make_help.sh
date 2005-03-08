546d3f3672c1148eb6c8962ca9ad636260f2e4fa
Mathieu Lutfy <mathieu@bidon.ca>
2005-03-08 09:17:32 +0000

    made it a bit more verbose

--- doc/templates/make_help.sh
+++ doc/templates/make_help.sh
@@ -20,5 +20,6 @@ if [ "X$1" = "X" -a "X$2" = "X" -a "X$3" = "X" ]; then
 	wget -q -O /dev/stdout $DOCROOT | sort | grep http | xargs -n 3 ./make_help.sh
 else
 	mkdir -p "$DEST/$2"
+	echo "$DEST/$2/$1.html"
 	wget -q -O "$DEST/$2/$1.html" "$3" 
 fi
