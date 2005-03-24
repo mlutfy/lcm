c5ea3eae77c796dcb131ed3f8c426823aed9d29e
Anastas Giokov <antzi@antzi.bg>
2005-03-24 20:03:51 +0000

    Implemented case_allow_modif global policy to case authors

--- legalcase/upd_case.php
+++ legalcase/upd_case.php
@@ -111,8 +111,9 @@ if (count($_SESSION['errors'])) {
 				id_case=$id_case,
 				id_author=$id_author,
 				ac_read=1,
-				ac_write=1,
-				ac_admin=1";
+				ac_write=1";
+		if (read_meta('case_allow_modif') == 'yes')
+			$q .= ", ac_edit=1, ac_admin=1";
 		$result = lcm_query($q);
 
 		// Get author information
