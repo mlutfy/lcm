39ee5a71a945b2288d1a9dbeb1119de70e37f73d
Mathieu Lutfy <mathieu@bidon.ca>
2005-03-25 13:15:01 +0000

    bumed db_version

--- legalcase/inc/inc_version.php
+++ legalcase/inc/inc_version.php
@@ -179,7 +179,7 @@ $lcm_version = 0.42;
 $lcm_version_shown = "0.4.2";
 
 // Current version of LCM database
-$lcm_db_version = 28;
+$lcm_db_version = 29;
 
 // Error reporting
 // error_reporting(E_ALL); // [ML] recommended for debug
