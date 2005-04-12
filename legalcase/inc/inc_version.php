ad0b8c2ea6e135aa195cbf96f4ed0ac0e9b0ac73
Mathieu Lutfy <mathieu@bidon.ca>
2005-04-12 07:26:23 +0000

    bumped version to 0.4.3

--- legalcase/inc/inc_version.php
+++ legalcase/inc/inc_version.php
@@ -177,10 +177,10 @@ if (@file_exists('inc/my_options.php'))
 	include('inc/my_options.php');
 
 // Current version of LCM
-$lcm_version = 0.42;
+$lcm_version = 0.43;
 
 // Current version of LCM shown on screen
-$lcm_version_shown = "0.4.2";
+$lcm_version_shown = "0.4.3";
 
 // Current version of LCM database
 $lcm_db_version = 31;
