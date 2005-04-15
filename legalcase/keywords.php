86b3f08e7b620afc0bb2084b44f010bc2d504e32
Mathieu Lutfy <mathieu@bidon.ca>
2005-04-15 08:01:30 +0000

    show_all..: removed 'remove_number_prefix()' since otherwise there is no quick way to know how keywords are ordered (and this is admin anyway)

--- legalcase/keywords.php
+++ legalcase/keywords.php
@@ -61,7 +61,7 @@ function show_all_keywords_type($type = '') {
 			foreach ($kw_all as $key => $kw) {
 				echo "\t<li>";
 				if ($suggest == $kw['name']) echo "<b>";
-				echo "<a href='?action=edit_keyword&amp;id_keyword=" . $kw['id_keyword'] . "' class='content_link'>". _T(remove_number_prefix($kw['title'])) . "</a>";
+				echo "<a href='?action=edit_keyword&amp;id_keyword=" . $kw['id_keyword'] . "' class='content_link'>". _T($kw['title']) . "</a>";
 				if ($kw['ac_author'] != 'Y') echo " (hidden) "; // TRAD
 				if ($suggest == $kw['name']) echo "</b>";
 				echo "</li>\n";
