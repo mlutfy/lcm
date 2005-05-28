28e96e5ff6a468c5adfe508b5d0f3333a9479c6f
Mathieu Lutfy <mathieu@bidon.ca>
2005-05-28 10:36:53 +0000

    in cal function for nav-bar, replaced sql 'LIKE' by date manip function

--- legalcase/inc/inc_presentation.php
+++ legalcase/inc/inc_presentation.php
@@ -318,7 +318,7 @@ function lcm_page_start($title = "", $css_files = "", $meta = '', $help_code = '
 			FROM lcm_app, lcm_author_app as a
 			WHERE (a.id_author=" . $GLOBALS['author_session']['id_author'] . "
 				AND lcm_app.id_app=a.id_app
-				AND start_time LIKE '" . date('Y-m-d') ."%')
+				AND DATE_FORMAT(start_time,'%Y-%m-%d') = CURDATE() )
 			ORDER BY reminder ASC";
 
 	$result = lcm_query($q);
