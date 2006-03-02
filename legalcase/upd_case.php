c553d0277aec220051c6de204e8582a48dfc8e60
Mathieu Lutfy <mathieu@bidon.ca>
2006-03-02 22:00:35 +0000

    minor changes in references to super-globals

--- legalcase/upd_case.php
+++ legalcase/upd_case.php
@@ -32,24 +32,38 @@ $_SESSION['errors'] = array();
 
 // Get form data from POST fields
 foreach($_POST as $key => $value)
-    $_SESSION['form_data'][$key]=$value;
+    $_SESSION['form_data'][$key] = $value;
 
 
 //
 // Clean (most of the) input
 //
 
-if (isset($_REQUEST['id_case']))
-	$id_case = intval($_REQUEST['id_case']);
-else
-	$id_case = 0;
+$id_case = _request('id_case', 0);
 
-$_SESSION['form_data']['title'] = clean_input($_SESSION['form_data']['title']);
-$_SESSION['form_data']['legal_reason'] = clean_input($_SESSION['form_data']['legal_reason']);
-$_SESSION['form_data']['alledged_crime'] = clean_input($_SESSION['form_data']['alledged_crime']);
-$_SESSION['form_data']['notes'] = clean_input($_SESSION['form_data']['notes']);
-$_SESSION['form_data']['status'] = clean_input($_SESSION['form_data']['status']);
-$_SESSION['form_data']['stage'] = clean_input($_SESSION['form_data']['stage']);
+//
+// Create client, if requested
+//
+
+if ($_REQUEST['add_client']) {
+	include_lcm('inc_obj_client');
+
+	$client = new LcmClient();
+	$errs = $client->save();
+
+	if (count($errs)) {
+		$_SESSION['errors'] = array_merge($_SESSION['errors'], $errs);
+	} else {
+		$_SESSION['form_data']['attach_client'] = $client->getDataInt('id_client', '__ASSERT__');
+	}
+}
+
+$_SESSION['form_data']['title'] = clean_input(_session('title'));
+$_SESSION['form_data']['legal_reason'] = clean_input(_session('legal_reason'));
+$_SESSION['form_data']['alledged_crime'] = clean_input(_session('alledged_crime'));
+$_SESSION['form_data']['notes'] = clean_input(_session('notes'));
+$_SESSION['form_data']['status'] = clean_input(_session('status'));
+$_SESSION['form_data']['stage'] = clean_input(_session('stage'));
 
 //
 // Check case data for validity
