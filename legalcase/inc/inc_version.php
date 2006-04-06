623c24246b9573ed9c1d53af458775eaaf68ab06
Mathieu Lutfy <mathieu@bidon.ca>
2006-04-06 21:34:02 +0000

    attempt to re-create errno using our errorHandler()

--- legalcase/inc/inc_version.php
+++ legalcase/inc/inc_version.php
@@ -410,6 +410,8 @@ if (!$PATH_TRANSLATED) {
 	else if ($DOCUMENT_ROOT && $SCRIPT_URL) $PATH_TRANSLATED = $DOCUMENT_ROOT.$SCRIPT_URL;
 }
 
+// For error handling after failed fopen/mkdir/etc
+$lcm_errormsg = '';
 
 function userErrorHandler($errno, $errmsg, $filename, $linenum, $vars) {
 	$dt = date("Y-m-d H:i:s (T)");
@@ -444,6 +446,9 @@ function userErrorHandler($errno, $errmsg, $filename, $linenum, $vars) {
 
 		lcm_debug("[dbg] " . $err, 2);
 	}
+
+	// set our custom errno, because PHP doesn't seem to have one!
+	$GLOBALS['lcm_errormsg'] = $errmsg;
 }
 
 $old_error_handler = set_error_handler("userErrorHandler");
