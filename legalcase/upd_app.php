dcb635dd1e6f0b236f563f0b867e0a7b6e19010c
Mathieu Lutfy <mathieu@bidon.ca>
2007-01-18 17:18:19 +0000

    fixes PGSQL bug

--- legalcase/upd_app.php
+++ legalcase/upd_app.php
@@ -2,7 +2,7 @@
 
 /*
 	This file is part of the Legal Case Management System (LCM).
-	(C) 2004-2005 Free Software Foundation, Inc.
+	(C) 2004-2007 Free Software Foundation, Inc.
 
 	This program is free software; you can redistribute it and/or modify it
 	under the terms of the GNU General Public License as published by the
@@ -161,7 +161,7 @@ if (count($_SESSION['errors'])) {
 		// Add ID of the creator
 		$q .= 'id_author = ' . $GLOBALS['author_session']['id_author'] . ',';
 		// Add the rest of the fields
-		$q .= "$fl, date_creation = NOW()";
+		$q .= "$fl, date_update = NOW(), date_creation = NOW()";
 
 		$result = lcm_query($q);
 
@@ -183,6 +183,7 @@ if (count($_SESSION['errors'])) {
 
 		lcm_query($q, true); // ignore errors
 		$_SESSION['errors']['author_added'] = "An author was added to the participants of this appointment."; // TRAD
+		// FIXME use $_SESSION['info'] instead
 	}
 
 	// Remove appointment participants (authors)
